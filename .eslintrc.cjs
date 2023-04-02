53e7047fa05394247d14feea0786b232e156b5ac
jruizsilva <ruizsilvajonathan@gmail.com>
2023-04-02 01:32:45 -0300

    eslint added

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard-with-typescript',
+    'prettier'
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json'
+  },
+  settings: { react: { version: 'detect' } },
+  plugins: ['react'],
+  rules: {}
+}
